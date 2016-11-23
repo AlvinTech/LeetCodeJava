a2e94aeed488737fb916928df25f5ec467032121
AlvinTech <alvalp@live.cn>
2016-11-23 10:29:17 +0800

    add 289 count primes

--- src/Count_Primes.java
+++ src/Count_Primes.java
@@ -3,23 +3,30 @@ import java.util.Arrays;
 /**
  * Created by Alvin on 16/11/23.
  */
+
+/*
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+* */
 public class Count_Primes {
     public int countPrimes(int n) {
-       boolean[] falg = new boolean[n];
-        for (int i =2 ; i <= Math.sqrt(n); i++){
+        boolean[] falg = new boolean[n];
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             //不要每个格子都遍历，也不要用 % 去计算能不能整除
-            for(int j =i; j < n ; j+=i){
-                if(falg[j]==false && j != i){
+            for (int j = i; j < n; j += i) {
+                if (falg[j] == false && j != i) {
                     falg[j] = true;
                 }
             }
         }
-        int count =0;
-//        System.out.println(Arrays.toString(falg));
-        for(int j =2; j < n ; j++){
-            if(falg[j] ==  false){
-//                System.out.println(j);
-                count++;}
+        int count = 0;
+        //        System.out.println(Arrays.toString(falg));
+        for (int j = 2; j < n; j++) {
+            if (falg[j] == false) {
+                //                System.out.println(j);
+                count++;
+            }
         }
         return count;
     }
