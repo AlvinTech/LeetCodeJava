aac4eaaf0934977ad368572149a680ce60d96b87
AlvinTech <alvalp@live.cn>
2016-12-02 14:35:22 +0800

    349 | Intersection of Two Arrays

---
+++ src/Intersection_of_Two_Arrays.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Alvin on 16/12/1.
+ */
+public class Intersection_of_Two_Arrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1.length == 0)
+            return nums1;
+        if (nums2.length == 0)
+            return nums2;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (!map.containsKey(nums1[i])) {
+                map.put(nums1[i], nums1[i]);
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i])) {
+                list.add(nums2[i]);
+                map.remove(nums2[i]);
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+}
